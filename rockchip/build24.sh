76c0ed94e7f962a0ce39843e0d813ba74d61c6db
Arthur97172 <arthur97172@outlook.com>
2026-06-22 15:32:44 +0800

    Update build24.sh

--- rockchip/build24.sh
+++ rockchip/build24.sh
@@ -22,8 +22,7 @@ echo "Building for profile: $PROFILE"
 # yml 传入的固件大小 ROOTFS_PARTSIZE
 echo "Building for ROOTFS_PARTSIZE: $ROOTFS_PARTSIZE"
 
-
-
+#查询是否包含第三方软件包
 if [ -z "$CUSTOM_PACKAGES" ]; then
   echo "⚪️ 未选择 任何第三方软件包"
 else
@@ -63,30 +62,6 @@ else
   arch aarch64_generic 10\n\
   arch aarch64_cortex-a53 15' repositories.conf
 fi
-
-
-#if [ -z "$CUSTOM_PACKAGES" ]; then
-#  echo "⚪️ 未选择 任何第三方软件包"
-#else
-#  # 下载 run 文件仓库
-#  echo "🔄 正在同步第三方软件仓库 Cloning run file repo..."
-#  git clone --depth=1 https://github.com/wukongdaily/store.git /tmp/store-run-repo
-
-#  # 拷贝 run/arm64 下所有 run 文件和ipk文件 到 extra-packages 目录
-#  mkdir -p /home/build/immortalwrt/extra-packages
-#  cp -r /tmp/store-run-repo/run/arm64/* /home/build/immortalwrt/extra-packages/
-
-#  echo "✅ Run files copied to extra-packages:"
-#  ls -lh /home/build/immortalwrt/extra-packages/*.run
-#  # 解压并拷贝ipk到packages目录
-#  sh shell/prepare-packages.sh
-#  ls -lah /home/build/immortalwrt/packages/
-#  # 添加架构优先级信息
-#  sed -i '1i\
-#  arch aarch64_generic 10\n\
-#  arch aarch64_cortex-a53 15' repositories.conf
-#fi
-
 
 # 输出调试信息
 echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始构建固件..."
