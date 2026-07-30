287dbb565bf0ea7bdceef0f5b6a4f11ae995b4f7
Arthur97172 <arthur97172@outlook.com>
2026-07-30 12:35:30 +0800

    Update apk-immortalwrt-custom-packages.sh

--- shell/apk-immortalwrt-custom-packages.sh
+++ shell/apk-immortalwrt-custom-packages.sh
@@ -12,7 +12,7 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES adguardhome luci-app-adguardhome luci-i18n-adg
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-amlogic luci-i18n-amlogic-zh-cn"
 
 # argon主题 (argon)
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-theme-argon"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-theme-argon luci-app-argon-config luci-i18n-argon-config-zh-cn"
 
 # aurora主题 (aurora)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-aurora-config luci-theme-aurora luci-i18n-aurora-config-zh-cn"
