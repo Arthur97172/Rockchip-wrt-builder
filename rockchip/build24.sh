50c19ee7d21d5b882617b88346a713155565d072
Arthur97172 <arthur97172@outlook.com>
2026-08-12 10:37:15 +0800

    Update build24.sh

--- rockchip/build24.sh
+++ rockchip/build24.sh
@@ -72,8 +72,8 @@ PACKAGES=""
 
 # [核心系统]
 PACKAGES="base-files libc libgcc uci ubus dropbear logd mtd opkg bash htop curl wget ca-bundle ca-certificates"
-PACKAGES="$PACKAGES -dnsmasq dnsmasq-full firewall4 nftables kmod-nft-offload"
-PACKAGES="$PACKAGES ip-full ipset iw ppp ppp-mod-pppoe -wpad-basic-mbedtls wpad-openssl"
+PACKAGES="$PACKAGES -dnsmasq dnsmasq-full firewall4 nftables kmod-nft-offload -odhcpd odhcpd-ipv6only odhcp6c"
+PACKAGES="$PACKAGES ip-full ipset iw ppp ppp-mod-pppoe -wpad-basic-mbedtls wpad-openssl luci-proto-ppp luci-proto-ipv6"
 
 # [硬件驱动 - 板载 PCIe]
 # 强制去重 ath10k 防止冲突；包含 Realtek 板载 2.5G (r8125) 和千兆 (r8169)
